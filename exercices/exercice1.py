dfc945a48779a30010db43545a74742244e11561
Damien Pla <damien.pla@live.fr>
2025-12-02 15:17:52 +0100

    Exercice1 : ajout du rapport

--- exercices/exercice1.py
+++ exercices/exercice1.py
@@ -1,4 +1,11 @@
 import pandas as pd
+
+
+def write_rapport(chaine):
+    with open("rapport.txt","a") as f:
+        f.write(chaine)
+    f.close()
+
 #Charger le fichier avec Pandas 
 df = pd.read_csv('ventes.csv')
 # 2. Ajouter une colonne montant_total (quantite × prix_unitaire)
@@ -7,13 +14,17 @@ print(df)
 # 3. Calculer le total des ventes par vendeur 
 par_vendeur = df.groupby('vendeur')['montant_total'].sum()
 print(f"Total des ventes par vendeur : {par_vendeur}")
+rapport = f"Total des ventes par vendeur : {par_vendeur}"
 
 # 4. Calculer le total des ventes par produit 
-par_produit = df.groupby('produit')['montant_total'].sum()
+par_produit = df.groupby('produit')['quantite'].sum()
 print(f"Total des ventes par produits : {par_produit}")
+rapport = rapport + f"Total des ventes par produits : {par_produit} "
+
 # 5. Identifier le top 3 des ventes (montant le plus élevé)
 top3 = df.sort_values('montant_total',ascending=False).head(3)
 print(top3)
+rapport = rapport + f"{top3}"
 #  6. Sauvegarder les résultats dans ventes_analysees.csv
 df.to_csv(
     'ventes_analysees.csv',
@@ -23,6 +34,10 @@ df.to_csv(
     header=True, # Inclure les en-têtes
     na_rep='NA', # Représentation des NaN
 )
+
+
+#bonus : on vas creer un rapport (fichier texte par exemple) pour mettre les reponses aux question 2,3 et 4 
+write_rapport(rapport)
 
 
 
